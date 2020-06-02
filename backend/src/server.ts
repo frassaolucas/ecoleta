3b063b31368c55bb300f052304abf7975bb38d1f
frassaolucas <frassao.lucas@gmail.com>
2020-06-02 08:49:14 -0300

    - Installed knex and sqlite to create project database; - configured database connection; - created specific routes file; - removed rest api tests;

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,9 @@
-import express, { response } from "express";
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-const users = ["Diego", "Cleiton", "Robson"];
-
-app.get("/users", (req, res) => {
-  const search = String(req.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  res.json(filteredUsers);
-});
-
-app.get("/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-
-  const user = users[id];
-
-  return res.json(user);
-});
-
-app.post("/users", (req, res) => {
-  const data = req.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return res.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
