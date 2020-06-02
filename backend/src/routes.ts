5c46a880b3515375f7d4c399c2344a65447ce2e7
frassaolucas <frassao.lucas@gmail.com>
2020-06-02 15:59:21 -0300

    - created route to create locations;

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import knex from "./database/connection";
 
 const routes = express.Router();
@@ -8,12 +8,39 @@ routes.get("/items", async (req, res) => {
 
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
 
   return res.json(serializedItems);
+});
+
+routes.post("/locations", async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  await knex("locations").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  return res.json({ success: true });
 });
 
 export default routes;
