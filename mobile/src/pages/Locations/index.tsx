e942de3dbf25f41b045130013e4e71d381cbce7d
frassaolucas <frassao.lucas@gmail.com>
2020-06-04 16:47:58 -0300

    - imported asset images; - created routes file and imported in the app; - created home, locations and detail pages; - finished home page styling;

---
+++ mobile/src/pages/Locations/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+// import { Container } from './styles';
+
+const Locations = () => {
+  return <View />;
+};
+
+export default Locations;
